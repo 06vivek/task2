580050aa10a72a1c2a0ae687729d4578188fd6ae
vivek patidar <70385245+06vivek@users.noreply.github.com>
2024-10-24 13:54:56 +0530

    Add files via upload

---
+++ task2.2.js
@@ -0,0 +1,16 @@
+  //2. After printing their value assign them as null and print it again.
+
+  var Name=null;
+  var Email=null;
+  var Age=null;
+  var address=null;
+  var phone_Number=null;
+  var Gender=null;
+  var married=null;
+  console.log("Name:- "+Name);
+  console.log("Email:- "+Email);
+  console.log("Age:- "+Age);
+  console.log("Address:- "+address);
+  console.log("Phone-No.:- "+phone_Number);
+  console.log("Gender:- "+Gender);
+  console.log("Married:- "+married);
---
+++ task2.3.js
@@ -0,0 +1,19 @@
+//3. Declare an object called employeeDetails and add all the above variables to this object now as keys with value as null and print this object.
+
+const employeedetails ={
+        Name:null,
+         Email:null,
+         Age:null,
+         address:null,
+         phone_Number:null,
+         Gender:null,
+         married:null
+}
+console.log("employeeDetails");
+console.log("Name:- "+employeedetails["Name"]);
+console.log("Email:- "+employeedetails["Email"]);
+console.log("Age:- "+employeedetails["Age"]);
+console.log("Address:- "+employeedetails["address"]);
+console.log("Phone-No.:- "+employeedetails["phone_Number"]);
+console.log("Gender:- "+employeedetails["Gender"]);
+console.log("Married:- "+employeedetails["married"]);
